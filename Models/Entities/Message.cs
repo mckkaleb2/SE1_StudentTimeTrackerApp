80f58d5558043a5a71640931937cb0f5dba64dd7
Robidas02 <robidas@etsu.edu>
2025-10-22 12:26:44 -0400

    Updated the Message and Timecard classes
    Finished the Timecard class and created the Message class.

---
+++ Models/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentTimeTrackerApp.Models.Entities
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Body { get; set; } = string.Empty;
+        // This will be the UserId of the sender grabbed with a GET before the message is sent.
+        public string Sender { get; set; } = string.Empty;
+        // This will be the UserId of the receiver grabbed with a GET through the connection likely using the Course class.
+        public string Recipient { get; set; } = string.Empty;
+    }
+}
--- Models/Entities/Timecard.cs
+++ Models/Entities/Timecard.cs
@@ -1,10 +1,16 @@
-﻿namespace StudentTimeTrackerApp.Models.Entities
+﻿using StudentTimeTrackerApp.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentTimeTrackerApp.Models.Entities
 {
     public class Timecard
     {
         public int Id { get; set; }
         public DateTime DateTime { get; set; }
-        // This is where location data would be stored. IF WE HAD ANY!
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
 
+        // Use a GET to grab the UserId when the Timecard is created to fill this field.
+        public string UserId { get; set; } = string.Empty;
     }
 }
