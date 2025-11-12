40336c71ab0c111562796b10c1ead9bca37ae8ae
Robidas02 <robidas@etsu.edu>
2025-11-12 12:28:24 -0500

    View changes and Find Course method.
    Altered the view of students and instructors and added a way to join Courses.

--- Services/CourseService.cs
+++ Services/CourseService.cs
@@ -14,23 +14,66 @@ namespace StudentTimeTrackerApp.Services
             _context = context;
         }
 
-        public void CreateCourse(string userId, string courseCode, int courseNum, int sectionNum)
+        public string CreateCourse(string userId, string courseCode, int courseNum, int sectionNum)
         {
+            bool isWorking = true;
+            string errorMessage = string.Empty;
             var instructor = _context.Instructors
                 .FirstOrDefault(i => i.UserId == userId);
 
             if (instructor == null) {
-                throw new InvalidOperationException("Instructor not found.");
+                isWorking = false;
+                errorMessage = "Instructor was not found. Please try again.";
+                // throw new InvalidOperationException("Instructor not found.");
             }
 
             var course = new Course
             {
+                
                 CourseCode = courseCode,
                 CourseNum = courseNum,
                 SectionNum = sectionNum,
             };
 
             course.Instructors.Add(instructor);
+
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+            return errorMessage;
+        }
+
+
+        public string FindCourse(string userId, string courseCode, int courseNum, int sectionNum)
+        {
+            bool isWorking = true;
+            string errorMessage = string.Empty;
+            var student = _context.Students
+                .FirstOrDefault(s => s.UserID == userId);
+
+            if (student == null) {
+                isWorking = false;
+                errorMessage = "No student found. Please try again.";
+                // throw new InvalidOperationException("No student found.");
+            }
+
+
+            var match = _context.Courses
+                .FirstOrDefault(c =>
+                c.CourseCode == courseCode &&
+                c.CourseNum == courseNum &&
+                c.SectionNum == sectionNum);
+
+            if (match == null)
+            {
+                isWorking = false;
+                errorMessage = "No course was found. Please try again.";
+                // throw new InvalidOperationException("No Course was found.");
+            }
+
+            student.Courses.Add(match);
+            match.Students.Add(student);
+            _context.SaveChanges();
+            return errorMessage;
         }
     }
 }
